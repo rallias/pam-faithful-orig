3232b101109f7993f2b4ea867075e764169ff698
Andrew Pietila <andrew@hax.technology>
2022-01-09 17:07:30 -0600

    Checkpoint before moving to N-API.

--- src/pam-faithful.cpp
+++ src/pam-faithful.cpp
@@ -5,6 +5,8 @@ struct pam_faithful_transaction
 {
     const char *pam_service_name;
     const char *pam_username;
+    Nan::Callback js_callback;
+    int num_message;
     const pam_message **message;
     pam_response **response;
     uv_mutex_t *pam_working;
@@ -12,17 +14,60 @@ struct pam_faithful_transaction
     bool pam_waiting_data;
     bool pam_done;
     int pam_retval;
+    pam_handle_t *handle;
 };
+
+void pam_start_check_resolve(pam_faithful_transaction *data)
+{
+}
+
+void pam_start_check(uv_check_t *handle)
+{
+    pam_faithful_transaction *data = static_cast<pam_faithful_transaction *>(handle->data);
+    if (!uv_mutex_trylock(data->pam_working))
+    {
+        if (data->pam_waiting_data)
+        {
+            Nan::HandleScope scope;
+            v8::Local<v8::Array> array = Nan::New<v8::Array>(data->num_message);
+            for (int i = 0; i < data->num_message; i++)
+            {
+                v8::Local<v8::Object> message = Nan::New<v8::Object>();
+                const pam_message *thisMessage = *(data->message + i);
+                Nan::Set(message, Nan::New<v8::String>("msg_style").ToLocalChecked(), Nan::New<v8::Number>(thisMessage->msg_style));
+                Nan::Set(message, Nan::New<v8::String>("msg").ToLocalChecked(), Nan::New<v8::String>(thisMessage->msg).ToLocalChecked());
+                Nan::Set(array, i, message);
+            }
+
+            v8::Local<v8::Value> args[1] = {array};
+
+            auto retData = data->js_callback.Call(1, args);
+            if (retData->IsPromise())
+            {
+            }
+        }
+        else if (data->pam_done)
+        {
+            uv_check_stop(handle);
+        }
+        else
+        {
+            uv_mutex_unlock(data->pam_working);
+        }
+    }
+}
 
 int pam_start_converser(int num_msg, const pam_message **msg, pam_response **resp, void *appdata_ptr)
 {
     pam_faithful_transaction *data = static_cast<pam_faithful_transaction *>(appdata_ptr);
+    data->num_message = num_msg;
     data->message = msg;
     data->response = resp;
     data->pam_waiting_data = true;
     uv_mutex_unlock(data->pam_working);
     uv_sem_wait(data->js_callback_working);
     uv_mutex_lock(data->pam_working);
+    data->pam_waiting_data = false;
     return PAM_SUCCESS;
 }
 
@@ -37,12 +82,13 @@ void pam_start_thread(uv_work_t *req)
     pam_handle_t *handle;
 
     int retval = pam_start(data->pam_service_name, data->pam_username, conversation, &handle);
-    data->pam_done = true;
     data->pam_retval = retval;
+    data->handle = handle;
+    data->pam_done = true;
     uv_mutex_unlock(data->pam_working);
 }
 
-// pam_start = (service: string, user: string, convCallback: ({message_style: int, message: string}) => Promise<string>)|string => Promise<pam_handle>
+// pam_start = (service: string, user: string, convCallback: ({msg_style: int, msg: string}[]) => Promise<string[]>)|string[] => Promise<pam_handle>
 NAN_METHOD(PamStart)
 {
     if (info.Length() < 3)
@@ -73,13 +119,26 @@ NAN_METHOD(PamStart)
     v8::Isolate *isolate = v8::Isolate::GetCurrent();
 
     v8::String::Utf8Value serviceV8Str(isolate, serviceVal);
-    std::string service(*serviceV8Str);
+    const std::string service(*serviceV8Str);
 
     v8::String::Utf8Value userV8Str(isolate, userVal);
-    std::string user(*userV8Str);
+    const std::string user(*userV8Str);
+
+    v8::Local<v8::Function> convCallbackLocalFunc = v8::Local<v8::Function>::Cast(convCallbackVal);
 
     uv_loop_t *default_loop = uv_default_loop();
-    pam_faithful_transaction *transaction = (pam_faithful_transaction *)uv_malloc_func(sizeof(pam_faithful_transaction));
+    pam_faithful_transaction *transaction = new pam_faithful_transaction;
+    transaction->pam_username = user.c_str();
+    transaction->pam_service_name = service.c_str();
+    transaction->js_callback.Reset(convCallbackLocalFunc);
+
+    uv_mutex_init(transaction->pam_working);
+    uv_sem_init(transaction->js_callback_working, 0);
+
+    uv_work_t *req = new uv_work_t;
+    req->data = transaction;
+
+    uv_queue_work(default_loop, req, pam_start_thread, NULL);
 
     // Course of action.
     // We create the shared data, a pam_faithful_transaction.
