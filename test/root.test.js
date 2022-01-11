666ce03ed2a0f76e51366dff6f7b56272610bef1
Andrew Pietila <andrew@hax.technology>
2022-01-11 16:31:02 -0600

    Run root tests outside of NPM environment.
    It is NPM's opinion that they know better than all users, and that there
    should be no code running as root, even though there are legitimate use
    cases where code needs to be run as root, and that anybody who thinks
    otherwise needs to do a substantial amount of additional work in order
    to accomplish the same with npm, or eschew npm.

---
+++ test/root.test.js
@@ -0,0 +1,17 @@
+const pam_faithful = require("../index");
+const linuxUser = require('linux-sys-user').promise();
+const os = require('os');
+
+beforeAll(async () => {
+    await linuxUser.addUser({ username: "pam-faithful-user", create_home: true, shell: "/bin/bash" })
+    return linuxUser.setPassword("pam-faithful-user", "pam-faithful-password");
+});
+
+test('Tests are started as root.', () => {
+    const userInfo = os.userInfo();
+    expect(userInfo.uid).toEqual(0);
+});
+
+afterAll(async () => {
+    return linuxUser.removeUser("pam-faithful-user");
+});
