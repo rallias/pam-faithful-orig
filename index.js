139195a2217ea4e1a71b3ecf8d164084038d7fd6
Andrew Pietila <andrew@hax.technology>
2022-01-11 15:23:12 -0600

    Initial testing shenanigans.

---
+++ index.js
@@ -0,0 +1,3 @@
+const pam_faithful = require('bindings')('pam_faithful');
+
+module.exports = pam_faithful;
---
+++ test/pre-sudo.test.js
@@ -0,0 +1,5 @@
+const pam_faithful = require('../index');
+
+test('pam_faithful loads', () => {
+    expect(pam_faithful).toBeDefined();
+})
