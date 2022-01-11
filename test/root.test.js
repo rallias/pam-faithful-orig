c78964263dd19b13043dcb721c42eae05f348589
Andrew Pietila <andrew@hax.technology>
2022-01-11 17:28:16 -0600

    Trying to figure out WHY creating a user takes so long.

--- test/root.test.js
+++ test/root.test.js
@@ -2,14 +2,12 @@ const pam_faithful = require("../index");
 const linuxUser = require('linux-sys-user').promise();
 const os = require('os');
 
+
+jest.setTimeout(60000);
+
 beforeAll(async () => {
-    try {
-        console.log(await linuxUser.addUser({ username: "pam-faithful-user", create_home: true, shell: "/bin/bash" }));
-        console.log(await linuxUser.setPassword("pam-faithful-user", "pam-faithful-password"));
-    } catch (err) {
-        console.log(err);
-        throw (err);
-    }
+    await linuxUser.addUser({ other_args: "-l", username: "pam-faithful-user", create_home: true, shell: "/bin/bash" });
+    await linuxUser.setPassword("pam-faithful-user", "pam-faithful-password");
 });
 
 test('Tests are started as root.', () => {
